bf698a96884114947166b5cf9fe9dca2bba74c9b
Michael <161510080+Michaelguru@users.noreply.github.com>
2025-02-07 21:54:19 +0100

    Add files via upload

--- JAVASCRIPT/about.js
+++ JAVASCRIPT/about.js
@@ -2,3 +2,20 @@ function toggleMenu() {
     const element = document.querySelector(".nav");
     element.classList.toggle ('visible');
 }
+document.addEventListener("DOMContentLoaded", function(){
+    const sections = document.querySelectorAll('.section');
+    const options = {
+        threshold: 0.2
+    };
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
